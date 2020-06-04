c0e6b4769e070fcb7bf1656d4468e614150bcaa5
Ernesto Sanchez <erniesanchez619@gmail.com>
2020-06-04 15:47:15 -0700

    added carriage return to getpid

--- a1/bots.c
+++ a1/bots.c
@@ -16,7 +16,7 @@ int main(){
 FILE *fp;
 fp=fopen("QUOTE.txt","w");//opening file
 
-fprintf(fp, "PID: %d\n", getpid());//write PID to file
+fprintf(fp, "PID: %d\r \n", getpid());//write PID to file
 fclose(fp);//closing file
 return 0;
 }
