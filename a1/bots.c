92ef327fe3051ff1e6874721515be9ebe7559cee
Ernesto Sanchez <erniesanchez619@gmail.com>
2020-06-04 14:35:24 -0700

    write to file PID

--- a1/bots.c
+++ a1/bots.c
@@ -1,9 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
+#include <sys/types.h>
 
 int main(){
 FILE *fp;
-fp=fopen("QUOTE.txt","w");
-fclose(fp);
+fp=fopen("QUOTE.txt","w");//opening file
+
+fprintf(fp, "PID: %d\n", getpid());//write PID to file
+fclose(fp);//closing file
 return 0;
 }
