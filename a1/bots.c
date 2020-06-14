4d728a2c86f7fac360f3fe290d4e276c74770c3c
Ernesto Sanchez <erniesanchez619@gmail.com>
2020-06-14 16:52:56 -0700

    tid

--- a1/bots.c
+++ a1/bots.c
@@ -24,9 +24,10 @@ will be assigned a quote depending on their parity and print to QOUTE.txt period
 **/
 void *ThreadDoesThis(void *ptr){
 
-  long threadId = pthread_self();
+  int tid;
+  tid = (int)ptr;
   for(int j = 0; j < 8; j++){//iterate through threads
-    if(threadId % 2 == 0){//if thread id is even sleep(2)
+    if(tid % 2 == 0){//if thread id is even sleep(2)
       sleep(2);
     } else {//if thread is is odd sleep(3)
       sleep(3);
@@ -34,13 +35,13 @@ void *ThreadDoesThis(void *ptr){
 
     sem_wait(&FLAG);//wait semaphore
 
-    printf("Thread %d is running\n", pthread_self());//print out to console the running threads
+    printf("Thread %d is running\n", tid);//print out to console the running threads
     fp=fopen("QUOTE.txt","a");//append to file
 
-    if(threadId % 2 == 0){//if thread id is even
-      fprintf(fp,"Thread %lu: %s\r \n", pthread_self(), "Controlling complexity is the essence of computer programming. --Brian Kernigan");
+    if(tid % 2 == 0){//if thread id is even
+       fprintf(fp,"Thread %d: %s\r \n", tid, "Controlling complexity is the essence of computer programming. --Brian Kernigan");
     } else{//if thread is is odd
-      fprintf(fp,"Thread %lu: %s\r \n", pthread_self(), "Computer science is no more about computers than astronomy is about telescopes. --Edsger Dijkstra");
+        fprintf(fp,"Thread %d: %s\r \n", tid, "Computer science is no more about computers than astronomy is about telescopes. --Edsger Dijkstra");
     }
 
     fclose(fp);//close file
@@ -49,7 +50,7 @@ void *ThreadDoesThis(void *ptr){
 
   }
   pthread_exit(NULL);
-  printf("Task completed! Please check QOUTE.txt.");
+
 }
 
 /**
@@ -68,13 +69,14 @@ int main(void) {
 
   pthread_t threads[NUM_THREADS];
   for(int i = 1; i <= NUM_THREADS; i++){//creating necessary threads
-    pthread_create(&threads[i], NULL, ThreadDoesThis, NULL);
+    pthread_create(&threads[i], NULL, ThreadDoesThis, (void*)i);
   }
 
   for(int i=1; i<= NUM_THREADS;i++){
     pthread_join(threads[i],NULL);//wait for threads to terminate and clean up resources
   }
 
+  printf("Task completed! Please check QOUTE.txt.");
   pthread_exit(NULL);
   sem_destroy(&FLAG);//destroy semaphore
   return 0;
