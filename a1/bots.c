619281c00d1f9278bdf4875702d248de3fe19c66
Ernesto Sanchez <erniesanchez619@gmail.com>
2020-06-14 21:37:25 -0700

    removed a2

--- a1/bots.c
+++ a1/bots.c
@@ -49,7 +49,7 @@ void *ThreadDoesThis(void *ptr){
     sem_post(&FLAG);//release semaphore
 
   }
-  pthread_exit(NULL);
+  pthread_exit(NULL);//terminates thread
 
 }
 
@@ -77,7 +77,6 @@ int main(void) {
   }
 
   printf("Task completed! Please check QOUTE.txt.");
-  pthread_exit(NULL);
   sem_destroy(&FLAG);//destroy semaphore
   return 0;
 }
